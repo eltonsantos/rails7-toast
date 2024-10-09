ae083490bb61269e16201d611ca0b042fec08ed1
Elton Santos <eltonaxl@hotmail.com>
2024-10-09 10:23:19 -0300

    First commit

---
+++ config/initializers/rails_toastify.rb
@@ -0,0 +1,8 @@
+RailsToastify.setup do |config|
+  # config.framework = :tailwind # tailwind or :bootstrap
+  config.position = 'top-right' # top-right, top-center, top-left, bottom-right, bottom-center, bottom-left
+  config.animation = 'bounce' # bounce, slide, zoom, flip
+  config.duration = 5000 # Duration in milliseconds
+  config.theme = 'light' # light or dark
+  config.type = 'error' # info, success, error, warning, default
+end
